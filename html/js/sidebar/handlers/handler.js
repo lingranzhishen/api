c9ce81c190c3fb99ca7d41e3332284a45685cf48
HarryXie <harry_xie@live.com>
2016-10-04 16:21:00 +0800

    修改前端url

--- html/js/sidebar/handlers/handler.js
+++ html/js/sidebar/handlers/handler.js
@@ -11,7 +11,7 @@ var handler = {}
 var apiClick = function(e){
 	var dom=e.currentTarget;
 	var apiId=$(dom).attr("data-id");
-	var url='http://116.255.179.50:7000/admin/api/getDetail/'+appId;
+	var url='/admin/api/getDetail/'+appId;
 	$.post(url,{},function(data){
 		var description=data.data.description;		
 		var html = md.render(description);
@@ -21,7 +21,7 @@ var apiClick = function(e){
 
 handler.init = function(){
 
-	var url = 'http://116.255.179.50:7000/admin/api/getList/v1';
+	var url = '/admin/api/getList/v1';
 	$.post(url, {}, function(data){
 		// var data = JSON.parse('{"errno":0,"data":[{"apiPath":"getAuthTree","description":"hello api","hostCode":"uc","hostName":"用户中心","hostRestApi":"api/auth/getAuthTree.json","id":1,"name":"获取权限树","requestMethod":"POST","versionCode":"v1"}],"status":"ok"}')
 		var data = data.data;
