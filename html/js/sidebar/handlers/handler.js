744ef0c9851f17b97bd0f9585ae646b6a7f89514
HarryXie <harry_xie@live.com>
2016-10-04 17:40:09 +0800

    添加额外字段

--- html/js/sidebar/handlers/handler.js
+++ html/js/sidebar/handlers/handler.js
@@ -8,11 +8,28 @@ var SidebarBox = require('../components/sidebarbox')
 
 var handler = {}
 
+// requestMethod 调用方法
+// apiGatewayPath 网关调用地址
+// endPointPath  后台实际地址
+// maintainer 维护人
+// hostName 所属项目
+
 var apiClick = function(e){
 	var dom=e.currentTarget;
 	var apiId=$(dom).attr("data-id");
-	var url='/admin/api/getDetail/'+appId;
+	var url='/admin/api/getDetail/'+apiId;
 	$.post(url,{},function(data){
+		//api name
+		$("#apiName").text(data.name);
+		//requestMethod
+		$("#requestMethod").text(data.requestMethod);
+		//endPointPath
+		$("#endPointPath").text(data.endPointPath);
+		//maintainer
+		$("#maintainer").text(data.maintainer);
+		//hostName
+		$("#hostName").text(data.hostName);
+
 		var description=data.data.description;		
 		var html = md.render(description);
 		$("#apiDescription").html(html);
@@ -20,7 +37,7 @@ var apiClick = function(e){
 }
 
 handler.init = function(){
-
+	
 	var url = '/admin/api/getList/v1';
 	$.post(url, {}, function(data){
 		// var data = JSON.parse('{"errno":0,"data":[{"apiPath":"getAuthTree","description":"hello api","hostCode":"uc","hostName":"用户中心","hostRestApi":"api/auth/getAuthTree.json","id":1,"name":"获取权限树","requestMethod":"POST","versionCode":"v1"}],"status":"ok"}')
